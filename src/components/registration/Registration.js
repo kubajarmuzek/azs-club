419510346fae48301015b6c5c7cd83a115a4f87f
kubajarmuzek <102328573+kubajarmuzek@users.noreply.github.com>
2024-04-20 14:39:52 +0200

    registration form

---
+++ src/components/registration/Registration.js
@@ -0,0 +1,172 @@
+import React, { useState } from "react";
+import "./Registration.css";
+import logo from '../../logo.png'; 
+
+
+function Registration() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [name, setName] = useState("");
+  const [surname, setSurname] = useState("");
+  const [course, setCourse] = useState("");
+  const [semester, setSemester] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const [studyMode, setStudyMode] = useState("");
+  const [errors, setErrors] = useState({});
+
+  const validateForm = () => {
+    const errors = {};
+
+    if (!email.trim()) {
+      errors.email = "Email jest wymagany";
+    } else if (!/\S+@\S+\.\S+/.test(email)) {
+      errors.email = "Niepoprawny adres email";
+    }
+
+    if (!name.trim()) {
+      errors.name = "Imię jest wymagane";
+    }
+
+    if (!surname.trim()) {
+      errors.surname = "Nazwisko jest wymagane";
+    }
+
+    if (!course.trim()) {
+      errors.course = "Kierunek studiów jest wymagany";
+    }
+
+    if (!semester.trim()) {
+      errors.semester = "Semestr jest wymagany";
+    }
+
+    if (!studyMode) {
+      errors.studyMode = "Forma studiów jest wymagana";
+    }
+
+    if (!password.trim()) {
+      errors.password = "Hasło jest wymagane";
+    } else if (password.length < 6) {
+      errors.password = "Hasło musi mieć conajmniej 6 znaków";
+    } else if (
+      !/(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]/.test(
+        password
+      )
+    ) {
+      errors.password =
+        "Hasło musi zawierać: małą literę, dużą literę, znak specjalny i cyfrę";
+    }
+
+    if (password !== confirmPassword) {
+      errors.confirmPassword = "Hasła są różne";
+    }
+
+    setErrors(errors);
+    return Object.keys(errors).length === 0;
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const isValid = validateForm();
+    if (isValid) {
+      // TODO Submit
+    }
+  };
+
+  return (
+    <div className="Registration">
+        <img src={logo} alt="Logo" className="logo" />
+      <h2>Zarejestruj się</h2>
+      <div>
+        <label>Adres Email</label>
+        <input
+          type="text"
+          id="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        {errors.email && <div className="error">{errors.email}</div>}
+      </div>
+      <div>
+        <label>Imię</label>
+        <input
+          type="text"
+          id="name"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
+        {errors.name && <div className="error">{errors.name}</div>}
+      </div>
+      <div>
+        <label>Nazwisko</label>
+        <input
+          type="text"
+          id="surname"
+          value={surname}
+          onChange={(e) => setSurname(e.target.value)}
+        />
+        {errors.surname && <div className="error">{errors.surname}</div>}
+      </div>
+      <div>
+        <label>Semestr</label>
+        <input
+          type="text"
+          id="semester"
+          value={semester}
+          onChange={(e) => setSemester(e.target.value)}
+        />
+        {errors.semester && <div className="error">{errors.semester}</div>}
+      </div>
+      <div>
+        <label>Kierunek Studiów</label>
+        <input
+          type="text"
+          id="course"
+          value={course}
+          onChange={(e) => setCourse(e.target.value)}
+        />
+        {errors.course && <div className="error">{errors.course}</div>}
+      </div>
+      <div>
+        <label>Forma studiów</label>
+
+        <select
+          value={studyMode}
+          onChange={(e) => setStudyMode(e.target.value)}
+        >
+          <option value=""></option>
+          <option value="stacjonarne">Stacjonarne</option>
+          <option value="niestacjonarne">Niestacjonarne</option>
+          <option value="online">Online</option>
+        </select>
+        {errors.studyMode && <div className="error">{errors.studyMode}</div>}
+      </div>
+      <div>
+        <label>Hasło</label>
+        <input
+          type="password"
+          id="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        {errors.password && <div className="error">{errors.password}</div>}
+      </div>
+      <div>
+        <label>Powtórz Hasło</label>
+        <input
+          type="password"
+          id="confirmPassword"
+          value={confirmPassword}
+          onChange={(e) => setConfirmPassword(e.target.value)}
+        />
+        {errors.confirmPassword && (
+          <div className="error">{errors.confirmPassword}</div>
+        )}
+      </div>
+      <button type="submit" onClick={handleSubmit}>
+        Zarejestruj
+      </button>
+    </div>
+  );
+}
+
+export default Registration;
